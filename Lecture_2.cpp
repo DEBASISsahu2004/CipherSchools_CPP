d33cd1e17ab27032a346c235f0d65181562d38c9
Debasis Sahu <113534109+DEBASISsahu2004@users.noreply.github.com>
2023-10-06 20:53:05 +0530

    Add files via upload

---
+++ Lecture_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>  // This line means Header
+
+using namespace std;  // std makes sure that cout is a part of iostream
+
+int main(){         // We have written a main function
+	
+	cout << "Hello world";
+	
+	return 0;
+}
+
+// Single line command
+/* This is
+Multi line command
+*/
