28639b1662b26f6e3bf3219ec6556d19e3d42916
Debasis Sahu <113534109+DEBASISsahu2004@users.noreply.github.com>
2023-10-06 22:16:16 +0530

    Add files via upload

---
+++ Lecture_4 II/Lecture_4_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	float a;
+	cin>>a;
+	if(a>0.01){
+		cout<<"The condition in if statement is correct";  //will only be executed if the statement in the paranthesis is correct
+	}
+	
+	return 0;
+}
