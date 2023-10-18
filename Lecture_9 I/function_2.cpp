a5b8ab92cd7f3c8e373ccdad841aa11dc5c34a31
Debasis <sahurocky524@gmail.com>
2023-10-18 07:58:30 +0530

    Functions
    Implementation of functions

---
+++ Lecture_9 I/function_2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int compare(int a, int b){
+	if(a>b){
+		return a;
+	}else{
+		return b;
+	}
+}
+
+//int compare(int a, int b){
+//	if(a==5){
+//		return 1;
+//	}
+//	return 0;
+//}
+
+int main(){
+	int a,b;
+	a=1,b=5;
+	cout<<compare(a,b);
+	return 0;
+}
---
+++ Lecture_9 I/function_3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b){
+	int c = a+b;
+	return c;
+}
+
+int main(){
+	int a,b;
+	a=3;
+	b=5;
+	int c;
+	c = sum(a,b);
+	cout<<c;
+	return 0;
+}
