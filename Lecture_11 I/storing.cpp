d0b118a4b41ae23482c37dcf9edc32eaa6342373
Debasis <sahurocky524@gmail.com>
2023-10-18 08:42:48 +0530

    Size
    Get the size of value a data type can store in variable

---
+++ Lecture_11 I/storing.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	// 1 byte = 8 bits
+	
+	cout<<"The size of int is: "<<sizeof(int)<<endl;   // this gives the size of int in byte
+	cout<<"The size of long long int is: "<<sizeof(long long int);   // this gives the size of long long in byte
+	
+	return 0;
+}
