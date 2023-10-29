896b17d91d18eaff746f7b54ee3480f4eef8e56f
Debasis <sahurocky524@gmail.com>
2023-10-29 13:59:02 +0530

    stack
    used stack to store data and performed some operations

---
+++ STL/stack.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    stack<int> a;
+    
+    a.push(5);
+    a.push(4);
+    a.push(3);
+
+    cout << a.top() << endl;
+
+    a.pop();
+
+    if(a.empty() == 0){
+        cout << "The stack is not empty." << endl;
+    }else{
+        cout << "Stack is empty" << endl;
+    }
+
+    a.pop();
+    a.pop();
+
+    if(a.empty() == 0){
+        cout << "The stack is not empty." << endl;
+    }else{
+        cout << "Stack is empty" << endl;
+    }
+
+}
